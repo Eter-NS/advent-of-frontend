7a9eca3c3ef29f1485124728f17d3aec3a5745c5
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2023-12-08 21:14:39 +0100

    Finished day 8

---
+++ tasks/2023-12-08/index.ts
@@ -0,0 +1,98 @@
+import { Country, countries } from "./countries";
+
+type Priority = "high" | "medium" | "low";
+
+export interface Letter {
+	content: string;
+	country: "pl" | "de" | "us";
+	priority: Priority;
+}
+
+type LetterSort = {
+	sort(letters: Letter[]): Letter[];
+};
+
+type SortingStrategy = PriorityStrategy | LengthStrategy | CountryStrategy;
+
+export class LetterSorter {
+	#sortingModule: SortingStrategy;
+
+	constructor(sorter: SortingStrategy) {
+		this.#sortingModule = sorter;
+	}
+
+	sortLetters(letters: Letter[]): Letter[] {
+		return this.#sortingModule.sort(letters);
+	}
+}
+
+export class PriorityStrategy implements LetterSort {
+	public sort(letters: Letter[]): Letter[] {
+		return letters.sort(
+			(a, b) =>
+				this.#convertPriorityToNumber(b.priority) -
+				this.#convertPriorityToNumber(a.priority)
+		);
+	}
+
+	#convertPriorityToNumber(priority: Priority): number {
+		const priorityToNumberTable: { [key in Priority]: number } = {
+			high: 2,
+			medium: 1,
+			low: 0,
+		};
+		switch (priority) {
+			case "high":
+				return priorityToNumberTable.high;
+			case "medium":
+				return priorityToNumberTable.medium;
+			case "low":
+				return priorityToNumberTable.low;
+			default:
+				throw new Error("Unexpected object: " + priority);
+		}
+	}
+}
+
+export class LengthStrategy implements LetterSort {
+	public sort(letters: Letter[]): Letter[] {
+		return letters.sort((a, b) => a.content.length - b.content.length);
+	}
+}
+
+interface CountryIndex {
+	[key: string]: Country;
+}
+
+export class CountryStrategy implements LetterSort {
+	#countryEastPositionRegistry: CountryIndex = {};
+
+	constructor() {
+		countries.forEach((country) => {
+			this.#countryEastPositionRegistry[country.countryCode] = country;
+		});
+	}
+
+	public sort(letters: Letter[]): Letter[] {
+		return letters.sort(
+			(a, b) =>
+				this.#convertCountryNameIntoEastPosition(b.country) -
+				this.#convertCountryNameIntoEastPosition(a.country)
+		);
+	}
+
+	#convertCountryNameIntoEastPosition(countryName: string): number {
+		return this.#countryEastPositionRegistry[countryName.toUpperCase()]
+			.east;
+	}
+}
+
+// const sorter = new LetterSorter(new CountryStrategy());
+
+// let letters: Letter[] = [
+// 	{ content: "Hi", country: "us", priority: "medium" },
+// 	{ content: "Halo", country: "de", priority: "low" },
+// 	{ content: "Cześć", country: "pl", priority: "high" },
+// ];
+// const sortedLetters = sorter.sortLetters(letters);
+// console.log(sortedLetters);
